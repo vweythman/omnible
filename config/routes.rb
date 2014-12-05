793133f8c65bab193485e8088dcb92ac8067eeb0
Valree Weythman <vweythman@gmail.com>
2014-12-05 02:05:09 -0800

    add dynamic to static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
+  get 'static_pages/about'
+
   resources :works
 
   resources :users
+
+  root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
