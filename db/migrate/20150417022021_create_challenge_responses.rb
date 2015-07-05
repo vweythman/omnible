9868b90cedd80ad9d9edfd7200f3c16ff26275ef
Valree Weythman <vweythman@gmail.com>
2015-07-05 15:20:14 -0700

    cleaned migrations folder

--- db/migrate/20150417022021_create_challenge_responses.rb
+++
@@ -1,9 +0,0 @@
-class CreateChallengeResponses < ActiveRecord::Migration
-  def change
-    create_table :challenge_responses do |t|
-      t.belongs_to :caller, polymorphic: true, index: true
-      t.belongs_to :response, polymorphic: true, index: true
-      t.timestamps
-    end
-  end
-end
