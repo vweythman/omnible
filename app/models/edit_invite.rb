515cb6984ae1df194e1ec493eebaa05dd5a3d23c
Valree Weythman <vweythman@gmail.com>
2015-07-10 17:24:32 -0700

    rename invited editor to edit invite

--- app/models/edit_invite.rb
+++ app/models/edit_invite.rb
@@ -1,4 +1,4 @@
-class InvitedEditor < ActiveRecord::Base
+class EditInvite < ActiveRecord::Base
   belongs_to :user
   belongs_to :editable
 end
--- test/models/edit_invite_test.rb
+++ test/models/edit_invite_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class InvitedEditorTest < ActiveSupport::TestCase
+class EditInviteTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
