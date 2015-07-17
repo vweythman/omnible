4d96dd6264533e47aca58ded67667a2629997de6
Valree Weythman <vweythman@gmail.com>
2015-07-17 12:56:40 -0700

    added index tests

--- test/models/viewable_test.rb
+++ test/models/viewable_test.rb
@@ -38,14 +38,29 @@ class ViewableTest < ActiveSupport::TestCase
   end
 
   test "should keep blocked users from viewing and be mostly public" do
+    # show
     assert_not @erik.viewable?(@sirka)
     assert @erik.viewable?(@randa)
     assert @erik.viewable?(nil)
+
+    # index
+    chars1 = Character.for_anons
+    chars2 = Character.unblocked_for @randa
+    chars3 = Character.unblocked_for @sirka
+
+    assert chars1.include? @erik
+    assert chars2.include?(@erik), "#{chars2.inspect} + #{@randa.blockers.inspect}"
+    assert_not chars3.include? @erik
   end
 
   test "should be public" do
+    # show
     assert @jane.viewable?(nil)
     assert @jane.viewable?(@randa)
     assert @jane.viewable?(@cinders)
+
+    # index
+    charlist = Character.for_anons
+    assert charlist.include? @jane
   end
 end
