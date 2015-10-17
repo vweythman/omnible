fc7bc268d29c1bd5ab8b1d3078b8a77a84bb01c3
Valree M. Weythman <vweythman@gmail.com>
2015-10-16 17:46:08 -0700

    added tagging information for forms

--- app/decorators/work_decorator.rb
+++ app/decorators/work_decorator.rb
@@ -3,6 +3,8 @@ class WorkDecorator < EditableDecorator
 	delegate_all
 	decorates_association :rating
 
+	# HEADINGS AND IDENTIFICATION
+	# ------------------------------------------------------------
 	def creation_title
 		"Create Work"
 	end
@@ -15,6 +17,8 @@ class WorkDecorator < EditableDecorator
 		h.content_tag :h1 do "Edit" end
 	end
 
+	# ABOUT
+	# ------------------------------------------------------------
 	def summary
 		object.summary || ""
 	end
@@ -30,6 +34,35 @@ class WorkDecorator < EditableDecorator
 		h.content_tag :p, class: "status " + state.downcase do
 			state
 		end
+	end
+
+	# RELATED MODELS
+	# ------------------------------------------------------------
+	def character_form_list()
+		@all_characters ||= Appearance.organize(self.appearances)
+	end
+
+	def cohort_names_by(role)
+		all_characters = character_form_list.with_indifferent_access
+		character_group = all_characters[role]
+		character_group.nil? ? [] : character_group.map(&:name)
+	end
+
+	def cohort_heading_by(role)
+		cheading = self.narrative? ? role + " Characters" : role + " (People)"
+		cheading.titleize
+	end
+
+	def location_heading
+		self.narrative? ? "Settings" : "Subject (Location)"
+	end
+
+	def tag_names
+		self.tags.map(&:name)
+	end
+
+	def place_names
+		self.places.map(&:name)
 	end
 	
 end
