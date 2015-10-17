be025e95a1cd49274fc57f8f419d5967b77b9e95
Valree M. Weythman <vweythman@gmail.com>
2015-10-16 18:07:35 -0700

    added update for method

--- app/models/identifier.rb
+++ app/models/identifier.rb
@@ -15,20 +15,39 @@
 
 class Identifier < ActiveRecord::Base
 
-  # SCOPES 
-  # ------------------------------------------------------------
-  scope :not_among_for, ->(character_id, identifiers) { where("character_id = ? AND name NOT IN (?)", character_id, identifiers)}
-  scope :are_among_for, ->(character_id, identifiers) { where("character_id = ? AND name IN (?)", character_id, identifiers)}
+	# SCOPES 
+	# ------------------------------------------------------------
+	scope :not_among_for, ->(character_id, identifiers) { where("character_id = ? AND name NOT IN (?)", character_id, identifiers)}
+	scope :are_among_for, ->(character_id, identifiers) { where("character_id = ? AND name IN (?)", character_id, identifiers)}
 
-  # ASSOCIATIONS
-  # ------------------------------------------------------------
-  belongs_to :character
+	# ASSOCIATIONS
+	# ------------------------------------------------------------
+	belongs_to :character
 
-  # PUBLIC METHODS
-  # ------------------------------------------------------------
-  # Heading - defines the main means of addressing the model
-  def heading 
-    name
-  end
+	# PUBLIC METHODS
+	# ------------------------------------------------------------
+	# Heading - defines the main means of addressing the model
+	def heading 
+		name
+	end
+
+	# CLASS METHODS
+	# ------------------------------------------------------------
+	def self.update_for(model, list)
+		if list.length > 0
+			Identifier.transaction do
+				remove  = Identifier.not_among_for(model.id, list).destroy_all
+				current = Identifier.are_among_for(model.id, list).pluck(:name)
+
+				to_be_added = list - current
+
+				to_be_added.each do |name|
+					Identifier.where(character_id: model.id, name: name).first_or_create
+				end
+			end
+		else
+			model.identifiers.destroy_all
+		end
+	end
 
 end
