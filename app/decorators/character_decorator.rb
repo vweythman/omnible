5a7344c576ead7bd53f5fc440bc0d3684eed54a5
Valree M. Weythman <vweythman@gmail.com>
2015-10-16 17:53:27 -0700

    pulled together character about into info page

--- app/decorators/character_decorator.rb
+++ app/decorators/character_decorator.rb
@@ -1,17 +1,18 @@
-class CharacterDecorator < Draper::Decorator
+class CharacterDecorator < EditableDecorator
 	delegate_all
 
-	# Define presentation-specific methods here. Helpers are accessed through
-	# `helpers` (aka `h`). You can override attributes, for example:
-	#
-	#   def created_at
-	#     helpers.content_tag :span, class: 'time' do
-	#       object.created_at.strftime("%a %m/%d/%y")
-	#     end
-	#   end
-
+	# HEADINGS
+	# ------------------------------------------------------------
 	def heading
 		name
+	end
+
+	def creation_title
+		"Create Character"
+	end
+
+	def editing_title
+		name + " (Unsaved Draft)"
 	end
 
 	def link_to_original
@@ -22,12 +23,50 @@ class CharacterDecorator < Draper::Decorator
 		end
 	end
 
+	# ABOUT
+	# ------------------------------------------------------------
 	def list_aliases
-		h.content_tag :p, class: 'identifiers' do
-			identifiers.map { |identifier| identifier.name }.join(", ")
+		if identifiers.size > 0
+			h.content_tag :p, class: 'identifiers' do identifiers.pluck(:name).join(", ") end
 		end
 	end
 
+	def variant_names
+		identifiers.pluck(:name).join("; ")
+	end
+
+	def viewpoints
+		if @viewpoints.nil?
+			prejudices  = self.prejudices.includes(:identity).decorate
+			opinions    = self.opinions.includes(:recip).decorate
+			@viewpoints = prejudices + opinions
+		end
+		return @viewpoints
+	end
+
+	def connections
+		@connections ||= InterconnectionsDecorator.decorate(self.interconnections)
+	end
+
+	def clone_list
+		h.content_tag :ul, class: "clones" do
+			clones.each do |clone|
+				h.concat descent_tree(clone)
+			end
+		end
+	end
+
+	def descent_tree(clone)
+		clone = clone.decorate
+		link  = h.link_to clone.name, clone
+		node  = clone.clones.empty? ? "" : clone.clone_list
+		h.content_tag :li do
+			link + node
+		end
+	end
+
+	# OPINIONS
+	# ------------------------------------------------------------
 	def public_opinion_status
 		@rep_amt ||= reputations.size
 
@@ -51,6 +90,63 @@ class CharacterDecorator < Draper::Decorator
 				data: reputations.uniques.map{|p| {x: p.fondness, y: p.respect, r: p.count} }
 			}
 		]
+	end
+
+	# ASIDE
+	# ------------------------------------------------------------
+	def aside
+		identities  = self.identities.alphabetic.includes(:facet).decorate
+		possessions = PossessionsDecorator.decorate(self.possessions.includes(:item, :generic))
+
+		if identities.length > 0 && possessions.can_list?
+			h.content_tag :aside do
+				if identities.can_list?
+					h.concat identities.subheading
+					h.concat identities.list
+				end
+
+				if possessions.can_list?
+					h.concat possessions.items.html_safe
+				end
+			end
+		end
+	end
+
+	# RELATED MODELS
+	# ------------------------------------------------------------
+	def pagination(user = nil)
+		prv = h.content_tag :li, class: 'prev'    do link_to_prev end
+		slf = h.content_tag :li, class: 'current' do self.name end
+		nxt = h.content_tag :li, class: 'next'    do link_to_next end
+		h.content_tag :ol, class: 'pagination' do
+			prv + slf + nxt 
+		end
+	end
+
+	def link_to_prev(user = nil)
+		prv = self.prev_character(user)
+		unless prv.nil?
+			h.link_to "&laquo; #{prv.name}".html_safe, prv
+		end
+	end
+
+	def link_to_next(user = nil)
+		nxt = self.next_character(user)
+		unless nxt.nil?
+			h.link_to "#{nxt.name} &raquo;".html_safe, nxt
+		end
+	end
+
+	def replication_bar
+		dup = h.link_to "Duplicate", h.replicate_path(self), method: :post
+		con = h.link_to("Connect to Original Variant", h.edit_clone_path(self))
+		rol = h.content_tag :a do "Create a Roleplay Copy" end
+
+		links = self.is_a_clone? ? (dup + rol) : (con + dup + rol)
+
+		h.content_tag :nav, class: 'toolkit replication' do
+			links
+		end
 	end
 
 end
