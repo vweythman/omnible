31717354cfd9a7e7c0e6a4bb378433ea46d47ffe
Valree M. Weythman <vweythman@gmail.com>
2015-10-16 18:11:54 -0700

    created identities decorator

---
+++ app/decorators/identities_decorator.rb
@@ -0,0 +1,57 @@
+class IdentitiesDecorator < Draper::CollectionDecorator
+
+	# HEADINGS AND IDENTIFICATION
+	# ------------------------------------------------------------
+	def heading
+		"Character Descriptors"
+	end
+
+	def subheading
+		h.content_tag :h2 do
+			"Overview"
+		end
+	end
+
+	def formid
+		"form_identities"
+	end
+
+	# ABOUT
+	# ------------------------------------------------------------
+	def can_list?
+		self.length > 0
+	end
+
+	def klass
+		:identities
+	end
+
+	def list
+		h.render partial: "shared/definitions", object: self.organized
+	end
+
+	def list_possible
+		list = Hash[self.facets.pluck(:name).zip(Array.new())]
+		if self.length > 0
+			ordered = self.organized
+			list.merge(ordered)
+		else
+			list
+		end
+	end
+
+	def organized
+		Identity.organize(object)
+	end
+
+	def field_class
+		"tags identities descriptions"
+	end
+
+	# RELATED MODELS
+	# ------------------------------------------------------------
+	def facets
+		@facets ||= Facet.all.order(:name)
+	end
+
+end
