ef653e83e4ec7cae6797f102035d5618db78ff53
Valree M. Weythman <vweythman@gmail.com>
2015-11-11 14:09:57 -0800

    added decoration and ordering

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,7 +1,7 @@
 class SubjectsController < ApplicationController
   def index
-  	@characters = Character.order('name').all
-  	@items      = Item.organized_all
+  	@characters = Character.not_pen_name.viewable_for(current_user).order('name').decorate
+  	@items      = Item.order('generics.name, items.name').includes(:generic).decorate
   	@places     = Place.order('forms.name, places.name').includes(:form).decorate
   end
 end
