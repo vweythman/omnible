02ba1a205c0bd1350e12597b956164a40994f810
Valree M. Weythman <vweythman@gmail.com>
2015-11-11 14:14:44 -0800

    added ordering to whole story display

--- app/controllers/works/whole_story_controller.rb
+++ app/controllers/works/whole_story_controller.rb
@@ -2,7 +2,7 @@ class Works::WholeStoryController < Works::StoriesController
 
 	def show
 		@story    = @work
-		@chapters = @story.chapters.decorate
+		@chapters = @story.chapters.ordered.decorate
 		find_comments
 	end
 
