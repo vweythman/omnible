5529b161647d103d5f3bf38523f1d35dd9879319
Valree M. Weythman <vweythman@gmail.com>
2015-11-11 14:18:39 -0800

    made characters listable

--- app/decorators/characters_decorator.rb
+++ app/decorators/characters_decorator.rb
@@ -1,3 +1,12 @@
-class CharactersDecorator < Draper::CollectionDecorator
+class CharactersDecorator < ListableCollectionDecorator
+
+	def creation_path
+		h.creation_toolkit "Character", h.new_character_path
+	end
+
+	private
+	def list_type
+		:links
+	end
 
 end
