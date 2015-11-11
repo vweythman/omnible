beaa6e8014257fad13cf33baab87656913c5dd67
Valree M. Weythman <vweythman@gmail.com>
2015-11-11 15:46:35 -0800

    added function aliases

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,5 +1,24 @@
 class UserDecorator < Draper::Decorator
-  delegate_all
+	delegate_all
 
+	def uploadable_types
+		[:article, :character, :item, :place, :story, :short_story]
+	end
+
+	def linkable_types
+		[:story_link]
+	end
+
+	def uploade_articles
+		self.uploaded_works.articles
+	end
+	
+	def uploade_short_stories
+		self.uploaded_works.short_stories
+	end
+
+	def uploade_stories
+		self.uploaded_works.stories
+	end
 
 end
