75d3765f9c9b34f275f6d706d0f16d24054d0d3b
Valree M. Weythman <vweythman@gmail.com>
2016-01-07 11:41:59 -0800

    added update method

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -17,12 +17,29 @@ class Tagging < ActiveRecord::Base
 
 	# SCOPES
 	# ------------------------------------------------------------
-	scope :not_among_for, ->(item_id, tag_ids) { where("item_id = ? AND tag_id NOT IN (?)", item_id, tag_ids) }
-	scope :are_among_for, ->(item_id, tag_ids) { where("item_id = ? AND tag_id IN (?)",     item_id, tag_ids) }
+	scope :not_among_for, ->(work, tag_ids) { where("work_id = ? AND tag_id NOT IN (?)", work.id, tag_ids) }
+	scope :are_among_for, ->(work, tag_ids) { where("work_id = ? AND tag_id IN (?)",     work.id, tag_ids) }
 
 	# ASSOCIATIONS
 	# ------------------------------------------------------------
 	belongs_to :work
 	belongs_to :tag
+
+	def self.update_for(model, list)
+		if list.length > 0
+			Tagging.transaction do
+				remove  = Tagging.not_among_for(model, list).destroy_all
+				current = Tagging.are_among_for(model, list).pluck(:id)
+
+				to_be_added = list - current
+
+				to_be_added.each do |id|
+					Tagging.where(work_id: model.id, tag_id: id).first_or_create
+				end
+			end
+		else
+			model.taggings.destroy_all
+		end
+	end
 
 end
