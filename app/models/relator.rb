3b0394249871b4570554eda21e9dc2b9f9ec6596
Valree M. Weythman <vweythman@gmail.com>
2016-01-07 11:43:35 -0800

    checked reversal of relator name

--- app/models/relator.rb
+++ app/models/relator.rb
@@ -18,6 +18,21 @@ class Relator < ActiveRecord::Base
 	# MODULES
 	# ------------------------------------------------------------
 	include Taggable
+	include EditableCategory
+
+	# SCOPES
+	# ------------------------------------------------------------
+	# - General Sorting
+	scope :has_reverse,   -> { where("right_name <> ''") }
+	scope :lacks_reverse, -> { where("right_name == ''") }
+
+	# - Name Lists
+	scope :left_names,  -> { pluck(:left_name) }
+	scope :right_names, -> { has_reverse.pluck(:right_name) }
+
+	# - Name Relators
+	scope :unreversible_lefts,  -> { has_reverse.select(:id, :left_name)  }
+	scope :unreversible_rights, -> { has_reverse.select(:id, :right_name) }
 
 	# ASSOCIATIONS
 	# ------------------------------------------------------------
@@ -27,26 +42,51 @@ class Relator < ActiveRecord::Base
 	# ------------------------------------------------------------
 	accepts_nested_attributes_for :interconnections, :allow_destroy => true
 
-	# METHODS
+	# CLASS METHODS
 	# ------------------------------------------------------------
-	# Heading - defines the main means of addressing the model
-	def heading
-		has_reverse? ? "#{left_name} & #{right_name}" : left_name.pluralize
+	def self.all_names
+		Relator.left_names + Relator.right_names
+	end
+
+	# PUBLIC METHODS
+	# ------------------------------------------------------------
+	# DefaultDirection - picks the direction of the relator
+	def default_direction
+		if has_left?
+			has_right? ? "either" : "left"
+		else
+			"right"
+		end
 	end
 
 	# LeftHeading - defines the left hand interconnection
 	def left_heading
-		"#{left_name}" unless left_name.nil?
+		left_name unless left_name.nil?
 	end
 
 	# RightHeading - defines the right hand interconnection
 	def right_heading
-		has_reverse? ? "#{right_name}" : left_heading
+		has_reverse? ? right_name : left_heading
 	end
 	
 	# HasReverse? - asks whether relator type has different left and right values
 	def has_reverse?
 		!(right_name.nil? || right_name.empty? || left_name.eql?(right_name))
 	end
+
+	def editable? user
+		user.staffer?
+	end
+
+	private
+
+	def has_left?
+		attributes.has_key? ('left_name')
+	end
+
+	def has_right?
+		attributes.has_key? ('right_name')
+	end
+
 
 end
