82338bbc918c6c33e744f8c1eb6d2d49669ab562
Valree M. Weythman <vweythman@gmail.com>
2016-01-07 12:11:17 -0800

    added boolean methods

--- app/models/character.rb
+++ app/models/character.rb
@@ -17,6 +17,7 @@
 #  allow_play      | boolean     | cannot be null
 #  allow_clones    | boolean     | cannot be null
 #  allow_as_clone  | boolean     | cannot be null
+#  can_connect     | boolean     | cannot be null
 #  is_fictional    | boolean     | default: true
 # ================================================================================
 
@@ -60,6 +61,11 @@ class Character < ActiveRecord::Base
 	has_many :memberships,  dependent: :destroy
 	has_many :possessions,  dependent: :destroy
 
+	has_many :main_appearances,    -> { Appearance.main }
+	has_many :mentions,            -> { Appearance.mentioned }
+	has_many :side_appearances,    -> { Appearance.side }
+	has_many :subject_appearances, -> { Appearance.subject }
+
 	has_many :reputations,            class_name: "Opinion",         dependent: :destroy, foreign_key: "recip_id"
 	has_many :left_interconnections,  class_name: "Interconnection", dependent: :destroy, foreign_key: "left_id"
 	has_many :right_interconnections, class_name: "Interconnection", dependent: :destroy, foreign_key: "right_id"
@@ -73,6 +79,11 @@ class Character < ActiveRecord::Base
 
 	has_many :groups,     through: :memberships
 	has_many :works,      through: :appearances
+
+	has_many :main_in,      through: :main_appearances
+	has_many :mentioned_in, through: :mentions
+	has_many :side_in,      through: :side_appearances
+	has_many :subject_in,   through: :subject_appearances
 
 	# - Has
 	has_many :details, dependent: :destroy, class_name: "CharacterInfo"
@@ -94,6 +105,8 @@ class Character < ActiveRecord::Base
 	accepts_nested_attributes_for :opinions,     :allow_destroy => true
 	accepts_nested_attributes_for :possessions,  :allow_destroy => true
 	accepts_nested_attributes_for :prejudices,   :allow_destroy => true
+	accepts_nested_attributes_for :left_interconnections,  :allow_destroy => true
+	accepts_nested_attributes_for :right_interconnections, :allow_destroy => true
 
 	# DEEP DUPLICATION
 	# ------------------------------------------------------------
@@ -104,13 +117,15 @@ class Character < ActiveRecord::Base
 		include_association :opinions
 		include_association :possessions
 		include_association :prejudices
+		include_association :left_interconnections
+		include_association :right_interconnections
 	end
 
 	# CLASS METHODS
 	# ------------------------------------------------------------
-	def self.batch_by_name(str, uploader)
+	def self.line_batch_by_name(str, uploader)
 		names = str.split(";")
-		ids   = Array.new
+		characters = Array.new
 
 		Character.transaction do 
 			found_characters = names.map { |name| 
@@ -118,11 +133,11 @@ class Character < ActiveRecord::Base
 				character = Character.where(name: name).first_or_create
 				character.uploader_id ||= uploader.id
 				character.save
-				ids << character.id
+				characters << character
 			}
 		end
 
-		ids
+		characters
 	end
 
 	# PUBLIC METHODS
@@ -131,12 +146,24 @@ class Character < ActiveRecord::Base
 	def heading
 		self.name
 	end
+	
+	def relation_set(use_id = true)
+		@relation_set ||= build_relation_set(use_id)
+	end
+
+	def get_rrset(relator_id)
+		relation_set.end_values(relator_id, self.id)
+	end
+
+	def get_lrset(relator_id)
+		relation_set.minor_keys(relator_id)
+	end
 
 	# GETTERS
 	# ............................................................
 	# Interconnections - finds both left and right interconnections
 	def interconnections
-		Interconnection.character_interconnections(self.id).order(:relator_id).includes(:relator, :left, :right)
+		Interconnection.relations_for(self.id).order(:relator_id).includes(:relator, :left, :right)
 	end
 
 	# NextCharacter - find next character alphabetically
@@ -179,17 +206,22 @@ class Character < ActiveRecord::Base
 
 	# QUESTIONS
 	# ............................................................
+	def connectable?(user)
+		self.can_connect || self.uploader_id = user.id
+	end
+
 	# IsPlayableCharacter - asks whether character is a player character
 	def is_playable_character?
+		!self.roleplayer.nil?
 	end
 
 	# Playable? - asks if character can be cloned for roleplay
 	def playable?
-		self.allow_play == 't' || self.allow_play == true
+		self.allow_play == true
 	end
 
 	def fictitious_person?
-		self.is_fictional == 't' || self.allow_play == true
+		self.allow_play == true
 	end
 
 	# PRIVATE METHODS
@@ -205,6 +237,14 @@ class Character < ActiveRecord::Base
 
 		self.editor_level    ||= Editable::PRIVATE
 		self.publicity_level ||= Editable::PUBLIC
+	end
+
+	def build_relation_set(use_id)
+		if use_id
+			DimensionSet.new(Interconnection.relations_for(self.id).simple_order, [:relator_id, :left_id, :right_id])
+		else
+			DimensionSet.new(Interconnection.relations_for(self.id).simple_order.includes(:left, :right), [:relator_id, :left_name, :right_name])
+		end
 	end
 
 end
