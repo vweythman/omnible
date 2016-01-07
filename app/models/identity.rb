fde55b076f2e05bfce830722d4f6adee955fe8a3
Valree M. Weythman <vweythman@gmail.com>
2016-01-07 11:46:58 -0800

    added alphabetic pagination to identity

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -25,16 +25,20 @@ class Identity < ActiveRecord::Base
 	scope :are_among, ->(names) { where("name IN (?)", names) }
 
 	# - Order
-	scope :alphabetic, -> { order('lower(name)') }
+	scope :alphabetic,         -> { order('lower(name) ASC') }
+	scope :reverse_alphabetic, -> { order('lower(name) DESC') }
+	scope :sorted_alphabetic,  -> { joins(:facet).order('lower(facets.name) ASC, lower(identities.name) ASC') }
 
 	# - Types
 	scope :ages,    -> { where(facet: 'age') }
 	scope :genders, -> { where(facet: 'gender') }
 
 	# - Associated
-	scope :next_in_facet, ->(identity) { where('facet_id = ? AND name > ?', identity.facet_id, identity.name).order('name ASC') }
-	scope :prev_in_facet, ->(identity) { where('facet_id = ? AND name < ?', identity.facet_id, identity.name).order('name DESC') }
-	scope :related,       ->(identity) { where("identities.id != ?", identity.id).joins(:descriptions).merge(Description.within(identity)).with_count(identity).select("COUNT(*) as amt")}
+	scope :alphabetic_next, ->(identity) { where('lower(name) > ?', identity.name.downcase).alphabetic }
+	scope :alphabetic_prev, ->(identity) { where('lower(name) < ?', identity.name.downcase).reverse_alphabetic }
+	scope :next_in_facet,   ->(identity) { where('facet_id = ? AND lower(name) > ?', identity.facet_id, identity.name.downcase).alphabetic }
+	scope :prev_in_facet,   ->(identity) { where('facet_id = ? AND lower(name) < ?', identity.facet_id, identity.name.downcase).reverse_alphabetic }
+	scope :related,         ->(identity) { where("identities.id != ?", identity.id).joins(:descriptions).merge(Description.within(identity)).with_count(identity).select("COUNT(*) as amt")}
 	
 	# - Divide
 	scope :with_count, ->(identity) { group("identities.id").order("COUNT(*) DESC").select("identities.*, (SELECT COUNT(*) FROM descriptions WHERE descriptions.identity_id = identities.id) as full_count")}
@@ -63,8 +67,23 @@ class Identity < ActiveRecord::Base
 		Identity.organize(list)
 	end
 
+	def self.faceted_line_batch(facet_id, str, uploader)
+		list  = Array.new
+		names = str.split(";")
+		Identity.transaction do
+			names.each do |name|
+				name.strip!
+				identity = Identity.where(name: name, facet_id: facet_id).first_or_create
+				list << identity
+			end
+		end
+		return list
+	end
+
 	# PUBLIC METHODS
 	# ------------------------------------------------------------
+	# GETTERS // SELF
+	# ............................................................
 	# Heading - defines the main means of addressing the model
 	def heading
 		"#{facet.name}: #{name}"
@@ -80,6 +99,18 @@ class Identity < ActiveRecord::Base
 		self
 	end
 
+	# GETTERS // RELATED MODELS
+	# ............................................................
+	# FacetedNext - next in facet
+	def alphabetic_next
+		@alphabetic_next ||= Identity.alphabetic_next(self).first
+	end
+
+	# FacetedPrev - previous in facet
+	def alphabetic_prev
+		@alphabetic_prev ||= Identity.alphabetic_prev(self).first
+	end
+
 	# FacetedNext - next in facet
 	def faceted_next
 		@faceted_next ||= Identity.next_in_facet(self).first
@@ -90,11 +121,20 @@ class Identity < ActiveRecord::Base
 		@faceted_prev ||= Identity.prev_in_facet(self).first
 	end
 
+	# Identity - finds identities commonly used together
+	def relatives(lim = 5)
+		Identity.related(self).limit(lim)
+	end
+
+	# SETTERS
+	# ............................................................
 	# Typify - set the facet type of the identity
 	def typify(name)
 		self.facet = Facet.where(name: name).first_or_create
 	end
 
+	# CALCS
+	# ............................................................
 	# CharacterPercent - percent of all characters with identity
 	def character_percent
 		1.0 * self.characters.count / Character.count * 100.0
@@ -105,14 +145,15 @@ class Identity < ActiveRecord::Base
 		1.0 * self.works.count / Work.count * 100.0
 	end
 
-	# Identity - finds identities commonly used together
-	def relatives(lim = 5)
-		Identity.related(self).limit(lim)
-	end
-
 	# UseCount - how often
 	def use_count
 		@usage_count ||= self.characters.count
+	end
+
+	# QUESTIONS
+	# ............................................................
+	def editable? user
+		user.staffer?
 	end
 	
 end
