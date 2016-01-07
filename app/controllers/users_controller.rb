ceeb118a13d140fc324f2b153354af7223e0b62c
Valree M. Weythman <vweythman@gmail.com>
2016-01-07 12:09:45 -0800

    added decorator to user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 	# GET
 	# ............................................................
 	def show
-		@user = User.find(params[:id])
+		@user = User.find(params[:id]).decorate
 	end
 
 end
