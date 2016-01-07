a4939cbcd90667b54712b654292b51772a30ded3
Valree M. Weythman <vweythman@gmail.com>
2016-01-07 12:15:23 -0800

    added show variables

--- app/controllers/users/dashboards_controller.rb
+++ app/controllers/users/dashboards_controller.rb
@@ -1,15 +1,24 @@
 class Users::DashboardsController < ApplicationController
 	
+	# FILTERS
+	# ------------------------------------------------------------
+	before_action :ensure_signed_in, only: [:show]
+	
 	# PUBLIC METHODS
 	# ------------------------------------------------------------
-	# GET
-	# ............................................................
 	def show
+		@user      = current_user.decorate
+		@pen_names = PenNamingsDecorator.decorate(current_user.pen_namings)
+		@skins     = @user.skins
+	end
+
+	# PRIVATE METHODS
+	# ------------------------------------------------------------
+	private
+	def ensure_signed_in
 		unless user_signed_in?
 			redirect_to new_user_session_path
 		end
-		@user      = current_user.decorate
-		@pen_names = PenNamingsDecorator.decorate(current_user.pen_namings)
 	end
 
 end
