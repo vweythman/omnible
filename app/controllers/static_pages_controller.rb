6c8a352a4b7d48c03106836214925b2fc75890dc
Valree M. Weythman <vweythman@gmail.com>
2016-01-07 12:09:25 -0800

    halved recent anthologies and works lists

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
-  	@anthologies = Anthology.recently_updated(10).decorate
-  	@works       = Work.recently_updated(10).decorate
+  	@anthologies = Anthology.recently_updated(5).decorate
+  	@works       = Work.local.recently_updated(5).decorate
   end
 
   def help
