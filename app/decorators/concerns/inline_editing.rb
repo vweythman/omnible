fa20d5eb6be4cab0e0bd3af90631e1c46bbc9f2d
Valree M. Weythman <vweythman@gmail.com>
2016-01-11 22:02:20 -0800

    added id methods for inline editing

--- app/decorators/concerns/inline_editing.rb
+++ app/decorators/concerns/inline_editing.rb
@@ -1,5 +1,6 @@
 module InlineEditing
 
+	# CREATE EDIT BARS
 	def edit_bar
 		h.inline_alteration_toolkit(self)
 	end
@@ -8,8 +9,30 @@ module InlineEditing
 		h.form_div_for_ajaxed_edit(klass, object.id)
 	end
 
-	def formid
-		"#" + h.edit_form_id(klass, object.id)
+	# SET ID
+	def tag_id
+		"#{klass}-#{object.id}"
+	end
+	
+	def kit_id
+		"#{klass}-edit-bar-for-#{self.id}"
+	end
+
+	def form_id
+		h.edit_form_id(klass, object.id)
+	end
+
+	# GET ID
+	def form_finder
+		"#" + form_id
+	end
+
+	def tag_finder
+		"#" + tag_id
+	end
+
+	def kit_finder
+		"#" + kit_id
 	end
 
 end
