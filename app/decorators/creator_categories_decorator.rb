251849b24ffb452aede3b5968c07da4160484779
Valree M. Weythman <vweythman@gmail.com>
2016-01-20 11:41:18 -0800

    added listable to creatorships

--- app/decorators/creator_categories_decorator.rb
+++ app/decorators/creator_categories_decorator.rb
@@ -1,3 +1,15 @@
 class CreatorCategoriesDecorator < Draper::CollectionDecorator
 
+	# MODULES
+	# ------------------------------------------------------------
+	include ListableCollection
+
+	def klass
+		:creator_categories
+	end
+
+	def list_type
+		:links
+	end
+
 end
