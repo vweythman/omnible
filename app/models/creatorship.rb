d32130cc22362b0e7291b023253d6850e01d1144
Valree M. Weythman <vweythman@gmail.com>
2016-02-02 06:42:39 -0800

    added to creatorship associations

--- app/models/creatorship.rb
+++ app/models/creatorship.rb
@@ -1,5 +1,12 @@
 class Creatorship < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :work
-  belongs_to :category, class_name: "CreatorCategory", inverse_of: :creatorships
+
+	scope :are_among_for, ->(cids) { where("creator_id IN (?)", cids)}
+
+	belongs_to :category, class_name: "CreatorCategory", inverse_of: :creatorships, foreign_key: "creator_category_id"
+	belongs_to :creator,  class_name: "Character"
+	belongs_to :work
+
+	belongs_to :pseudonyming, primary_key: "character_id", foreign_key: "creator_id"
+	has_one    :psuedonymer,  through: :pseudonyming, source: :user
+
 end
