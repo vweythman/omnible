4f4e91cb4208cf0f8d4a96609c860033e49ed70a
Valree M. Weythman <vweythman@gmail.com>
2016-02-06 16:50:39 -0800

    added response levels

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -10,11 +10,18 @@
 #  title           | string      | maximum of 250 characters
 #  uploader_id     | integer     | references user
 #  about           | string      | can be null
-#  response_level  | string      | who can response
+#  response_level  | string      | who can respond
 #  created_at      | datetime    | must be earlier or equal to updated_at
 #  updated_at      | datetime    | must be later or equal to created_at
 # ================================================================================
 
 class Challenge < ActiveRecord::Base
+	
+	UNPUBLISHED  = 'undecided'
+	FRIENDS_ONLY = 'friends only'
+	FRIENDS_N_FOLLOWERS = 'friends and followers'
+	EXCEPT_BLOCKED      = 'any user except blocked users'
+	ANYONE              = 'anyone'
+
   belongs_to :uploader
 end
