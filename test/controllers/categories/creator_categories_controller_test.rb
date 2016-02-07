375388fdb7605757469617a0f2ce25cd73794a5b
Valree M. Weythman <vweythman@gmail.com>
2016-02-07 00:56:27 -0800

    added category tests

--- test/controllers/categories/creator_categories_controller_test.rb
+++ test/controllers/categories/creator_categories_controller_test.rb
@@ -1,6 +1,137 @@
 require 'test_helper'
 
 module Categories
-  class FacetsControllerTest < ControllerTestCase
+  class CreatorCategoriesControllerTest < ControllerTestCase
+
+    setup do
+      @sirka   = users(:sirka)
+      @randa   = users(:randa)
+
+      @writer  = creator_categories(:writer)
+      @poem    = works_type_describers(:poem)
+      @article = works_type_describers(:article)
+    end
+
+    test "should redirect when not admin" do
+      sign_in @sirka
+      get :index
+      assert_redirected_to root_path
+    end
+
+    # GET
+    # ============================================================
+    test "should get index" do
+      sign_in @randa
+      
+      get :index
+
+      assert_response :success
+      assert_not_nil assigns(:creator_categories)
+    end
+
+    test "should show category" do
+      sign_in @randa
+
+      get :show, id: @writer
+
+      assert_response :success
+      assert_not_nil assigns(:creator_category)
+    end
+
+    test "should get new" do
+      sign_in @randa
+
+      get :new
+
+      assert_response :success
+      assert_not_nil assigns(:creator_category)
+    end
+
+    test "should get edit" do
+      sign_in @randa
+
+      get :edit, id: @writer
+
+      assert_response :success
+      assert_not_nil assigns(:creator_category)
+    end
+
+    # POST
+    # ============================================================
+    test "should create category and redirect" do
+      sign_in @randa
+      assert_equal 3, CreatorCategory.count
+
+      assert_difference('CreatorCategory.count') do
+        post :create, creator_category: { name: 'composer', agentive: 'composed by' }, format: 'html'
+      end
+
+      assert_redirected_to creator_category_path(assigns(:creator_category))
+    end
+
+    test "should create category and grab list" do
+      sign_in @randa
+
+      assert_difference('CreatorCategory.count') do
+        post :create, creator_category: { name: 'composer', agentive: 'composed by' }, format: 'js'
+      end
+
+      assert_response :success
+      assert_not_nil assigns(:creator_category)
+      assert_not_nil assigns(:creator_categories)
+    end
+
+    # PATCH/PUT
+    # ============================================================
+    test "should update category and redirect" do
+      sign_in @randa
+      patch :update, id: @writer, creator_category: { name: @writer.name, agentive: @writer.agentive, work_types: [@writer.type_describers.pluck(:id)] }
+      assert_redirected_to creator_category_path(assigns(:creator_category))
+    end
+
+    test "should update category without describers and render js file" do
+      sign_in @randa
+
+      patch :update, id: @writer, creator_category: { name: @writer.name, agentive: @writer.agentive }, format: 'js'
+
+      assert_response :success
+      assert_not_nil assigns(:creator_category)
+      assert_empty assigns(:creator_category).type_describers
+    end
+
+    test "should update category with new describer" do
+      sign_in @randa
+      assert_not @writer.connected? @poem
+
+      patch :update, id: @writer, creator_category: { name: @writer.name, agentive: @writer.agentive, work_types: [@poem.id] }, format: 'js'
+
+      assert_response :success
+      assert_not_nil assigns(:creator_category)
+      assert assigns(:creator_category).connected? @poem
+    end
+
+    test "should update category without old describer" do
+      sign_in @randa
+      assert @writer.connected? @article
+
+      patch :update, id: @writer, creator_category: { name: @writer.name, agentive: @writer.agentive, work_types: [@writer.type_describers.pluck(:id) - [@article]] }, format: 'js'
+
+      assert_response :success
+      assert_not_nil assigns(:creator_category)
+      assert_not assigns(:creator_category).connected? @article
+    end
+
+    # DELETE
+    # ============================================================
+    test "should destroy category" do
+      sign_in @randa
+
+      assert_difference('CreatorCategory.count', -1) do
+        delete :destroy, id: @writer
+      end
+
+      assert_redirected_to creator_categories_path
+    end
+
   end
 end
--- test/controllers/categories/facets_controller_test.rb
+++ test/controllers/categories/facets_controller_test.rb
@@ -1,6 +1,143 @@
 require 'test_helper'
 
 module Categories
-  class CreatorCategoriesControllerTest < ControllerTestCase
+  class FacetsControllerTest < ControllerTestCase
+
+    setup do
+      @sirka = users(:sirka)
+      @randa = users(:randa)
+      @age   = facets(:age)
+    end
+
+    # GET
+    # ============================================================
+    test "should get index" do
+      # :: setup
+      sign_in @randa
+      
+      # :: process
+      get :index
+
+      # :: result
+      assert_response :success
+      assert_not_nil assigns(:facets)
+    end
+
+    test "should show facet" do
+      # :: setup
+      sign_in @randa
+
+      # :: process
+      get :show, id: @age
+
+      # :: result
+      assert_response :success
+      assert_not_nil assigns(:facet)
+    end
+
+    test "should get new" do
+      # :: setup
+      sign_in @randa
+
+      # :: process
+      get :new
+
+      # :: result
+      assert_response :success
+      assert_not_nil assigns(:facet)
+    end
+
+    test "should get edit" do
+      # :: setup
+      sign_in @randa
+
+      # :: process
+      get :edit, id: @age
+
+      # :: result
+      assert_response :success
+      assert_not_nil assigns(:facet)
+    end
+
+    # POST
+    # ============================================================
+    test "should create facet and redirect" do
+      # :: setup
+      sign_in @randa
+      assert_equal 3, Facet.count
+
+      # :: process
+      assert_difference('Facet.count') do
+        post :create, facet: { name: 'status' }, format: 'html'
+      end
+
+      # :: result
+      assert_redirected_to facet_path(assigns(:facet))
+    end
+
+    test "should create facet and grab list" do
+      # :: setup
+      sign_in @randa
+
+      # :: process
+      assert_difference('Facet.count') do
+        post :create, facet: { name: 'status' }, format: 'js'
+      end
+
+      # :: result
+      assert_response :success
+      assert_not_nil assigns(:facet)
+      assert_not_nil assigns(:facets)
+    end
+
+    test "should redirect when not admin" do
+      sign_in @sirka
+      post :create, facet: { name: 'status' }, format: 'html'
+      assert_redirected_to facets_path
+    end
+
+    # PATCH/PUT
+    # ============================================================
+    test "should update facet and redirect" do
+      sign_in @randa
+      patch :update, id: @age, facet: { name: @age.name }
+      assert_redirected_to facet_path(assigns(:facet))
+    end
+
+    test "should update facet and render js file" do
+      # :: setup
+      sign_in @randa
+
+      # :: process
+      patch :update, id: @age, facet: { name: @age.name }, format: 'js'
+
+      # :: result
+      assert_response :success
+      assert_not_nil assigns(:facet)
+    end
+
+    # DELETE
+    # ============================================================
+    test "should destroy facet and redirect" do
+      sign_in @randa
+
+      assert_difference('Facet.count', -1) do
+        delete :destroy, id: @age
+      end
+
+      assert_redirected_to facets_path
+    end
+
+    test "should destroy facet" do
+      sign_in @randa
+
+      assert_difference('Facet.count', -1) do
+        delete :destroy, id: @age, format: 'js'
+      end
+
+      assert_response :success
+      assert_not_nil assigns(:facets)
+    end
+
   end
 end
