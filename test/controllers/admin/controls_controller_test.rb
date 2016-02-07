045f1df0917033735fa15e8f2cc50490dec90d0f
Valree M. Weythman <vweythman@gmail.com>
2016-02-07 00:56:11 -0800

    switched to sirka

--- test/controllers/admin/controls_controller_test.rb
+++ test/controllers/admin/controls_controller_test.rb
@@ -1,21 +1,24 @@
 require 'test_helper'
 
 class ControlsControllerTest < ControllerTestCase
+
   setup do
-    @letty = users(:letty)
+    @sirka = users(:sirka)
     @randa = users(:randa)
     @controller = Admin::ControlsController.new
+  end
+
+  test "should redirect" do
+    sign_in @sirka
+    get :show
+    assert_redirected_to root_path
   end
 
   test "should get show" do
     sign_in @randa
     get :show
     assert_response :success
+    assert_not_nil assigns(:user)
   end
 
-  test "should redirect" do
-    sign_in @letty
-    get :show
-    assert_redirected_to root_path
-  end
 end
--- test/controllers/categories/agentize_controller_test.rb
+++ test/controllers/categories/agentize_controller_test.rb
@@ -2,30 +2,45 @@ require 'test_helper'
 
 module Categories
   class AgentizeControllerTest < ControllerTestCase
+
     setup do
-      @letty   = users(:letty)
+      @sirka   = users(:sirka)
       @randa   = users(:randa)
 
       @poem    = works_type_describers(:poem)
       @article = works_type_describers(:article)
       
-      @writer  = creator_category(:writer)
+      @writer  = creator_categories(:writer)
     end
 
     test "should redirect" do
-      sign_in @letty
+      sign_in @sirka
       post :create, describer_id: @poem.id, creator_id: @writer.id
       assert_redirected_to root_path
     end
 
     test "should create connection" do
       sign_in @randa
+
+      count = @poem.creator_categories.size
+
+      assert_equal 0, @poem.creator_categories(:reload).size
       post :create, describer_id: @poem.id, creator_id: @writer.id, format: 'js'
+
+      assert_not_nil assigns(:describer)
+      assert_not_nil assigns(:creator_category)
+      assert_equal count + 1, assigns(:describer).creator_categories(:reload).size
     end
 
     test "should destroy connection" do
       sign_in @randa
-      post :destroy, describer_id: @poem.id, creator_id: @writer.id, format: 'js'
+      count = @article.creator_categories.size
+
+      delete :destroy, describer_id: @article.id, creator_id: @writer.id, format: 'js'
+
+      assert_not_nil assigns(:describer)
+      assert_not_nil assigns(:creator_category)
+      assert_equal count - 1, assigns(:describer).creator_categories(:reload).size
     end
 
   end
