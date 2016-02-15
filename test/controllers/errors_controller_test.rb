010e2012a018e0ab2cc490fe7450cee36439efc9
Valree M. Weythman <vweythman@gmail.com>
2016-02-14 19:37:57 -0800

    created error page tests

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -2,4 +2,29 @@ require 'test_helper'
 
 class ErrorsControllerTest < ControllerTestCase
 
+  test "should get 403 error page" do
+    get :e403
+    assert_response :success
+  end
+
+  test "should get 404 error page" do
+    get :e404
+    assert_response :success
+  end
+
+  test "should get 406 error page" do
+    get :e406
+    assert_response :success
+  end
+
+  test "should get 422 error page" do
+    get :e422
+    assert_response :success
+  end
+
+  test "should get 500 error page" do
+    get :e500
+    assert_response :success
+  end
+
 end
