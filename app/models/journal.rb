f680f9156732126ffb56432763ecbefac21d0069
Valree M. Weythman <vweythman@gmail.com>
2016-02-14 19:28:01 -0800

    fixed name

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -8,11 +8,11 @@ class Journal < Work
 	# ASSOCIATIONS
 	# ------------------------------------------------------------
 	has_many :articles, :inverse_of => :work, foreign_key: "work_id", class_name: "Note"
-	has_many :comments, :through => :notes
+	has_many :comments, :through => :articles
 
 	# NESTED ATTRIBUTION
 	# ------------------------------------------------------------
-	accepts_nested_attributes_for :notes, :allow_destroy => true
+	accepts_nested_attributes_for :articles, :allow_destroy => true
 
 	# PUBLIC METHODS
 	# ------------------------------------------------------------
