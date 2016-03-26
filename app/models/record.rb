fc35ac821d8c94ad383e02c607642e6161041a00
Valree M. Weythman <vweythman@gmail.com>
2016-03-25 18:37:43 -0700

    added medium to records

--- app/models/record.rb
+++ app/models/record.rb
@@ -5,9 +5,17 @@
 
 class Record < Work
 
-	# DELEGATED METHODS
+	# CALLBACKS
 	# ============================================================
-	delegate :medium, to: :metadata
+	before_create :set_categories
+
+	# ASSOCIATIONS
+	# ============================================================
+	has_one :medium_datum, ->{ RecordMetadatum.mediums }, foreign_key: "work_id", class_name: "RecordMetadatum"
+
+	# NESTED ATTRIBUTION
+	# ============================================================
+	accepts_nested_attributes_for :medium_datum
 
 	# PUBLIC METHODS
 	# ============================================================
@@ -23,6 +31,41 @@ class Record < Work
 
 	def viewable?(user)
 		true
+	end
+
+	# GETTERS 
+	# ------------------------------------------------------------
+	def tag_heading
+		title + " [#{taggable_medium}]"
+	end
+
+	def medium
+		medium_datum.value
+	end
+
+	def taggable_medium
+		searchable_metadata["medium"]
+	end
+
+	def searchable_metadata
+		@searchable_metadata ||= Hash[self.metadata.map{|m| [m.key, m.value]}]
+	end
+
+	def mediumize
+		if medium_datum.nil?
+			medium_datum       = RecordMetadatum.new
+			medium_datum.key   = "medium"
+			medium_datum.value = "Unsorted"
+		end
+	end
+
+	# PRIVATE METHODS
+	# ============================================================
+	private
+
+	# set default categories
+	def set_categories
+		self.status = 'unknown'
 	end
 
 end
