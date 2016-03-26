26e75acd27c3c2388af468e0a0015e8f100d1abf
Valree M. Weythman <vweythman@gmail.com>
2016-03-25 18:44:33 -0700

    added title tests

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -24,11 +24,23 @@ class WorkTest < ActiveSupport::TestCase
     assert work.metadata.pluck(:value).include? "Poem"
   end
 
+  test "should sort titles string by existence" do
+    existing_works, new_works_titles = Work.updated_tag_values(Work.all, "Deadly Storms;Star Wars [Franchise]; Star Wars [Film Series]; Fate of Fortune [Poem];Fate of Fortune [Short Story]")
+
+    assert_equal 2, existing_works.size
+    assert_equal 3, new_works_titles.split(";").size
+    assert existing_works.include? @short_story
+  end
+
   test "should find by title proper" do
     assert_equal @article,     Work.find_by_title_proper("Happiness Meta / indra's pen")
     assert_equal @journal,     Work.find_by_title_proper("Measure of Rainfall")
     assert_equal @short_story, Work.find_by_title_proper("Fate of Fortune [Short Story]")
     assert_not Work.find_by_title_proper("Fire and Ice")
+  end
+
+  test "should find with title values" do
+    assert_equal @short_story, Work.find_with_title_values("Fate of Fortune", "Short Story", nil)
   end
 
   test "should determine title from title proper" do
@@ -50,6 +62,7 @@ class WorkTest < ActiveSupport::TestCase
     assert_not Work.find_creator_name_by( "Fire and Ice" )
   end
 
+
   # TESTS :: PUBLIC METHODS
   # ============================================================
   # GETTERS
@@ -58,16 +71,27 @@ class WorkTest < ActiveSupport::TestCase
   	assert_equal @story.title, @story.heading
   end
 
+  test "should use type in tag heading" do
+    assert_equal "Fate of Fortune [Short Story]", @short_story.tag_heading
+    assert_equal "Dada [Film]", @record.tag_heading
+  end
+
+  test "should hash the metadata" do
+    assert @record.searchable_metadata.is_a?(Hash)
+    assert_equal "Film", @record.searchable_metadata["medium"]
+  end
+
+  test "should humanize type" do
+    assert_equal "Film", @record.medium
+    assert_equal "Film", @record.taggable_medium
+    assert_equal "Short Story", @short_story.categorized_type
+  end
+
   # QUESTIONS
   # ------------------------------------------------------------
   test "should be complete" do
     assert @article.complete?
     assert @short_story.complete?
-  end
-
-  test "should humanize type" do
-    assert_equal "Film", @record.medium
-    assert_equal "Short Story", @short_story.categorized_type
   end
 
 end
