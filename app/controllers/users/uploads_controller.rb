2969aa53d2c35f1b3733fcd53e50d3796499aefc
Valree M. Weythman <vweythman@gmail.com>
2016-10-15 02:06:00 -0700

    updated uploads index

--- app/controllers/users/uploads_controller.rb
+++ app/controllers/users/uploads_controller.rb
@@ -9,23 +9,29 @@ class Users::UploadsController < ApplicationController
 	# GET
 	# ------------------------------------------------------------
 	def index
-		resource_type = params[:resource_type]
+		@resource_type = params[:resource_type]
 
-		if resource_type.nil?
+		# ALL UPLOADS
+		if @resource_type.nil?
 			@uploads = current_user.all_uploads.sort_by! { |x| x.heading.downcase }
 
-		elsif resource_type == 'roleplay_character'
+		# ROLEPLAY CHARACTERS
+		elsif @resource_type == 'roleplay_character'
 			@uploads = current_user.roleplay_characters.alphabetical
 
-		elsif resource_type == 'work'
+		elsif @resource_type == 'anthology'
+			@uploads = current_user.anthologies.alphabetic
+
+		# ALL WORKS
+		elsif @resource_type == 'work'
 			@uploads = current_user.works.onsite
 
+		# WORKS BY TYPE
 		else
-			@uploads = current_user.works.by_type(resource_type.camelize)
+			@uploads = current_user.works.by_type(@resource_type.camelize)
 		end
-	end
 
-	def show
+		@uploads = Collectables::UploadsDecorator.decorate @uploads
 	end
 
 	# PRIVATE METHODS
