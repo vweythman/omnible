0c8f63ad9e0c1c3aab5aa4a4e6cd4428f49c554f
Valree M. Weythman <vweythman@gmail.com>
2016-10-27 11:18:45 -0700

    simplified trackings controller

--- app/controllers/trackings_controller.rb
+++ app/controllers/trackings_controller.rb
@@ -2,10 +2,13 @@ class TrackingsController < ApplicationController
 
 	before_action :signed_restriction
 
+	# PUBLIC METHODS
+	# ============================================================
+	# POST
+	# ------------------------------------------------------------
 	def create
-		tracked_type   = request.fullpath.split("/")[1].singularize
-		tracked_id     = params[tracked_type.downcase.underscore + '_id']
-		@to_be_tracked = tracked_type.classify.constantize.find(tracked_id)
+		tracked_params
+		@to_be_tracked = @tracked_type.classify.constantize.find(@tracked_id)
 
 		unless @to_be_tracked.uploader? current_user
 			current_user.track @to_be_tracked
@@ -16,18 +19,29 @@ class TrackingsController < ApplicationController
 		end
 	end
 
+	# DELETE
+	# ------------------------------------------------------------
 	def destroy
-		tracked_type = request.fullpath.split("/")[1].singularize
-		tracked_id   = params[tracked_type.downcase.underscore + '_id']
-		@tracked     = tracked_type.classify.constantize.find(tracked_id)
+		tracked_params
+		@being_tracked = @tracked_type.classify.constantize.find(@tracked_id)
 
-		if current_user.tracking? @tracked
-			current_user.untrack(@tracked)
+		if current_user.tracking? @being_tracked
+			current_user.untrack(@being_tracked)
 
 			respond_to do |format|
 				format.js { render layout: false }
 			end
 		end
+	end
+
+	# PRIVATE METHODS
+	# ============================================================
+	private
+
+	# Find Model Type and ID
+	def tracked_params
+		@tracked_type = request.fullpath.split("/")[1].singularize
+		@tracked_id   = params[@tracked_type.downcase.underscore + '_id']
 	end
 
 end
